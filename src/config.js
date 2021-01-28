27bae02815aa8c007f1ba982ba7f7951c011ee87
zeidalidiez <zeidalidiez@gmail.com>
2021-01-28 08:44:17 -0800

    frontend updates

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-    // API_ENDPOINT: 'https://fast-beyond-40504.herokuapp.com/vinyl',
-    API_ENDPOINT:'http://localhost:8000/vinyl',
-    TOKEN_KEY: 'vinyl-rock-auth-token'
+    API_ENDPOINT:'http://localhost:8000/covidapi',
+    TOKEN_KEY: 'spaced-repetition-jwt-secret'
 }
