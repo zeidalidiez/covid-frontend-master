0ceaba2cedfea6b54dfda4b0de899003fa07f571
zeidalidiez <zeidalidiez@gmail.com>
2021-01-28 17:11:03 -0800

    Update Walker.js

--- src/Components/Walker/Walker.js
+++ src/Components/Walker/Walker.js
@@ -11,8 +11,8 @@ export default class Walker extends Component {
                     <Link to={`/WalkersProfile/${walker.id}`}>
                         {walker.first_name}
                     </Link>
-                    <h4>AvailableTomorrow</h4>
-                    <p>Tomorrow @ 7 a.m</p>
+                    <h4>Available next day</h4>
+                    <p>Next day @ 7 a.m</p>
                 </li>
             </div>
         )
