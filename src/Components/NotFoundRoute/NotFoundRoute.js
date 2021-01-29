18875df70d3852b2aa78dacec07281319a9f003a
zeidalidiez <zeidalidiez@gmail.com>
2021-01-28 20:54:20 -0800

    Update NotFoundRoute.js

--- src/Components/NotFoundRoute/NotFoundRoute.js
+++ src/Components/NotFoundRoute/NotFoundRoute.js
@@ -5,7 +5,6 @@ class NotFoundRoute extends Component {
     return (
       <section>
         <h2>404 - Page not found</h2>
-        <p>Try going back to your previous page.</p>
       </section>
     );
   }
