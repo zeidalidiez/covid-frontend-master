bcd6133099df40a2a0b95a76814205386f585f0d
mismyl123 <mlalonde309@gmail.com>
2021-01-28 22:48:35 -0500

    tests pass

--- src/Components/Utilitys/Utils.test.js
+++ src/Components/Utilitys/Utils.test.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import { ServiceButtons, 
-         TimeButtons, 
-         SideNavButton, 
-         SideNavDrawer,
-         Backdrop ,
-         ServiceList } from '../Utilitys/Utils'
+import { ServiceButtons } from '../Utilitys/Utils'
 
 
 it('renders Utils without crashing', () => {
@@ -14,11 +9,6 @@ it('renders Utils without crashing', () => {
   ReactDOM.render(
     <BrowserRouter>
       <ServiceButtons/>
-      <TimeButtons/>
-      <SideNavButton/>
-      <SideNavDrawer/>
-      <Backdrop/>
-      <ServiceList/>
     </BrowserRouter>, div)
   ReactDOM.unmountComponentAtNode(div)
 })
