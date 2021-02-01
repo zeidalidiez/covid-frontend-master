ac4277bdfdee79abb0549e45ae0a8efaa65bc94a
zeidalidiez <zeidalidiez@gmail.com>
2021-02-01 12:48:36 -0800

    zz

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT:'https://coviddogwalking.herokuapp.com/covidapi',
+    API_ENDPOINT:'http://localhost:8000/covidapi',
     TOKEN_KEY: 'spaced-repetition-jwt-secret'
 }
